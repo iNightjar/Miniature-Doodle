66ebe9075e6af5933c05bf3a917be004d14d3556
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-13 20:15:41 +0200

    FBV, GET POST, GET PUT DELETE Co-authored-by: CodeCrader <sicarioeg4@gmail.com>

--- tickets/views.py
+++ tickets/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from .models import Guest, Movie, Reservation
-
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import Guest, GuestSerializer, Movie, Reservation
+from rest_framework import status, filters
 # Create your views here.
 
 #1 without rest framework, and no model query, FBV
@@ -37,5 +39,59 @@ def no_rest_from_model(request):
     return JsonResponse(response)
 
 
+# list == GET
+# create == POST
+# pk query == GET
 
 
+# update == PUT
+# Delete, Destroy == DELETE
+
+
+# 3 function based views
+
+#3.1 GET POST
+@api_view(['GET', 'POST'])
+def FBV_List(request):
+    # GET
+    if request.method == 'GET':
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data)
+    # POST
+    elif request.method == 'POST':
+        serializer = GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+#3.1 GET PUT DELETE
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def FBV_pk(request, pk):
+    try:
+        guest = Guest.objects.get(pk=pk)
+    except Guest.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    # GET
+    if request.method == 'GET':
+        # guests = Guest.objects.all()      No need to query the database
+        serializer = GuestSerializer(guest)
+        return Response(serializer.data)
+    
+    # PUT
+    elif request.method == 'PUT':
+        serializer = GuestSerializer(guest, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # DELETE
+    if request.method == 'DELETE':
+        # guests = Guest.objects.all()
+        # serializer = GuestSerializer(guests, many=True)
+        guest.delete()
+        return Response(statu=status.HTTP_204_NO_CONTENT)
