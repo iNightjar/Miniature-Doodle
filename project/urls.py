350061cfb75fa36eea12b3da41d703a359fbada8
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-13 20:17:31 +0200

    GET POST GET PUT DELETE restframework FBV @api_view
    > Co-authored-by: CodeCrader <sicarioeg4@gmail.com>

--- project/urls.py
+++ project/urls.py
@@ -5,10 +5,17 @@ from tickets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #1 ordinal route with django
+    # 1 ordinal route with django
     path('django/jsonresponsenomodel/', views.no_rest_no_model),
 
-    #2 no rest but django, queryset with json response
-    path('django/jsonresponsefrommodel/', views.no_rest_from_model)
+    # 2 no rest but django, queryset with json response
+    path('django/jsonresponsefrommodel/', views.no_rest_from_model),
+
+    # 3.1 GET POST from rest framework function based views @api_view
+    path('rest/fbv/', views.FBV_List),
+
+
+    # 3.2 GET PUT DELETE from rest framework function based views @api_view
+    path('rest/fbv/<int:pk>', views.FBV_pk)
 
 ]
