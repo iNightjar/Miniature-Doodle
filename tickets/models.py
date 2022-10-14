ad83b4374a0c33e846b2693a90051cde32e7b03c
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-14 15:28:54 +0200

    NewUsers TokenGenerated Authentication

--- tickets/models.py
+++ tickets/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Token model to generate token key for every guest(user) gets created
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
+
 
 # Guest - Movie - Reservation
-
-
 class Movie(models.Model):
     hall = models.CharField(max_length=10)
     movie = models.CharField(max_length=30) # extending max_length to 30
@@ -21,3 +25,12 @@ class Reservation(models.Model):
         Guest, related_name='reservation', on_delete=models.CASCADE)
     movie = models.ForeignKey(
         Movie, related_name='reservation', on_delete=models.CASCADE)
+
+
+
+
+# autogenerate tokens for newusers
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def TokenCreate(sender, instance, created, **Kwargs):
+    if created:
+        Token.objects.create(user=instance)
